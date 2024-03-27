775bff8e36797c797f794382135b69e39a49ad78
uta-ixm4307 <ixm4307@mavs.uta.edu>
2024-03-27 15:09:50 -0500

    outline of alpha-beta decision algorithm

--- Assignment_2/red_blue_nim.py
+++ Assignment_2/red_blue_nim.py
@@ -1,7 +1,7 @@
 import sys
 
 class State:
-    def __init__(self, remainBMarbles, remainRMarbles, version, player, depth, computer, human):
+    def __init__(self, remainBMarbles, remainRMarbles, version, player, depth, computer, human, alpha, beta):
         self.remainBMarbles = int(remainBMarbles)
         self.remainRMarbles = int(remainRMarbles)
         self.version = version
@@ -9,6 +9,8 @@ class State:
         self.depth = int(depth)
         self.computer = computer
         self.human = human
+        self.alpha = int(alpha)
+        self.beta = int(beta) 
 
 class Player:
     def __init__(self, entity, playerBMarbles, playerRMarbles):
@@ -20,6 +22,7 @@ class Action:
     def __init__(self, marble_color, marbles_taken):
         self.marble_color = marble_color
         self.marbles_taken = int(marbles_taken)
+        self.utility = 0
 
 def player_score(player):
     score = (player.playerBMarbles*3) + (player.playerRMarbles*2) 
@@ -27,18 +30,28 @@ def player_score(player):
 
 def take_action(state,action,computer,human): # After taking an action, the game state changes. Configure the game state 
     # Take Action Message
-    if(action.marble_color == "B"):
+    if((action.marble_color == "B") and ((state.player).entity == "computer")):
+        if(action.marbles_taken == 1):
+            print("Computer takes", action.marbles_taken, "blue marble.\n")
+        else:
+            print("Computer takes", action.marbles_taken, "blue marbles.\n")
+    elif((action.marble_color == "R") and ((state.player).entity == "computer")):
+        if(action.marbles_taken == 1):
+            print("Computer takes", action.marbles_taken, "red marble.\n")
+        else:
+            print("Computer takes", action.marbles_taken, "red marbles.\n")
+    elif((action.marble_color == "B") and ((state.player).entity == "human")):
         if(action.marbles_taken == 1):
             print("You take", action.marbles_taken, "blue marble.\n")
         else:
             print("You take", action.marbles_taken, "blue marbles.\n")
-    elif(action.marble_color == "R"):
+    elif((action.marble_color == "R") and ((state.player).entity == "human")):
         if(action.marbles_taken == 1):
             print("You take", action.marbles_taken, "red marble.\n")
         else:
             print("You take", action.marbles_taken, "red marbles.\n")
 
-    # Things that will change: remaining marbles, player marbles, and the current player
+    # Things that will change: remaining marbles, player marbles, and the current player, alpha and beta values set to -infinity and +infinity for computer's turn
     if((action.marble_color == "B")): # Marble picked is blue
         state.remainBMarbles = state.remainBMarbles - action.marbles_taken
     else: # Marble picked is red
@@ -47,6 +60,10 @@ def take_action(state,action,computer,human): # After taking an action, the game
         state.player = human
     else: # current player is human, next turn is computer.
         state.player = computer
+
+    state.alpha = -(sys.maxsize)
+    state.beta = sys.maxsize
+
     return state
 
 def print_marble_stacks(state):
@@ -54,17 +71,6 @@ def print_marble_stacks(state):
     print("Remaining Blue Marbles: " , state.remainBMarbles)
     print("Remaining Red Marbles: " , state.remainRMarbles)
     print("-------------------------------\n")   
-
-
-# def minimax_decision(state):
-
-# def max_value(state, alpha, beta):
-#     v
-#     return v
-
-# def min_value(state, alpha, beta):
-#     v
-#     return v
 
 def is_integer(s):
     return s.isdigit()
@@ -137,41 +143,126 @@ def end_of_game(state):
     elif(((state.player).entity == "human") and (state.version == "misere")):
         print("You Won")
 
-# def main():
-if(len(sys.argv)<3):
-    print("Not enough arguments\n")
-    sys.exit()
+def successor_state(action, state): # works similarily like take_action(), used in the alpha-beta decision algorithm for the computer's decision. Returns the successor state after taking an action
+    computer = Player("computer", (state.computer).playerBMarbles, (state.computer).playerRMarbles)
+    human = Player("human", (state.human).playerBMarbles, (state.human).playerRMarbles)
 
-arguments = sys.argv[1:] # Arguments passed: <num red> <num blue> <version> <first-player> <depth>.
-computer = Player("computer", 0, 0)
-human = Player("human", 0, 0)
-state = State(arguments[0], arguments[1], "standard", computer, 0, computer, human) # Default state attributes set.
+    if((state.player).entity == "human"): # current player is human, successor state is computer's turn.
+        successor = State(state.remainBMarbles, state.remainRMarbles, state.version, computer, state.depth+1, computer, human, state.alpha, state.beta)
+    else: # current player is human, successor state is computer's turn.
+        successor = State(state.remainBMarbles, state.remainRMarbles, state.version, human, state.depth+1, computer, human, state.alpha, state.beta)
 
-config_init_state(arguments,state,computer,human)
+    if((action.marble_color == "B")): # Marble picked is blue
+        successor.remainBMarbles = successor.remainBMarbles - action.marbles_taken
+    else: # Marble picked is red
+        successor.remainRMarbles = successor.remainRMarbles - action.marbles_taken
 
-print("\n_____________________Nim Game_____________________")
-while((state.remainBMarbles>0) and (state.remainRMarbles>0)):
-    if((state.player).entity == "computer"):
-        print("\n=====Computer=====")
-        state.player = human
-    else: #player turn
-        print("\n=====Player=====")
-        print_marble_stacks(state)
+    return successor
 
-        marble_color = query_marble_color()
-        marbles_taken = query_marbles_taken(state, marble_color)
+def possible_actions(state)
+    # 
 
-        action = Action(marble_color, marbles_taken)
+def successors(state)
+    actionsList = possible_actions(state) # returns a list of possible actions for a given state
+    successorStateList = []
+    for action in actionsList: 
+        successorStateList.apend(successor_state(action,state))
+    return successorStateList
 
-        state = take_action(state,action,computer,human)
-end_of_game(state)
+def utility(state):
+
+def terminal_test(test):
+
+def max_util(actions): # returns the higest utility among a list of actions
+
+# def min_util(actions):
+
+def find(util, actions) # Maps the given utility to the first corresponding action in the action list. Returns found action
+
+def max_value(state):
+    util = utility(state)
+    if (terminal_test(state) == True)
+        return util
+    v = -(sys.maxsize)
+
+    successorStateList = successors(state)
+
+    for s in successorStateList:
+        v = max(v, min_value(s))
+        if(v>=state.beta)
+            return v
+        state.alpha = max(state.alpha,v)
+    
+    return v
+
+def min_value(state):
+    util = utility(state)
+    if (terminal_test(state) == True)
+        return util
+    v = sys.maxsize
+
+    successorStateList = successors(state)
+
+    for s in successorStateList:
+        v = min(v, max_value(s))
+        if(v<=state.alpha)
+            return v
+        state.beta = min(state.beta,v)
+    
+    return v
+
+def alpha_beta_decision(state):
+    # Configure the alpha and beta values
+    state.alpha = -(sys.maxsize)
+    state.beta = sys.maxsize
+
+    actionsList = possible_actions(state) # returns a list of possible actions for a given state
+    for action in actionsList: 
+        action.utility = min_value(successor_state(action, state)) # for each possible action we configure it's payoff (utility)
+    utility = max_util(actionsList) # returns the higest utility among the actions in actionList
+    return find(util, actionsList) # returning the action with the highest utility
 
 
 
 
 
-# if __name__ == "__main__":
-#     main()
+def main():
+    if(len(sys.argv)<3):
+        print("Not enough arguments\n")
+        sys.exit()
+
+    arguments = sys.argv[1:] # Arguments passed: <num red> <num blue> <version> <first-player> <depth>.
+    computer = Player("computer", 0, 0)
+    human = Player("human", 0, 0)
+    state = State(arguments[0], arguments[1], "standard", computer, 0, computer, human, -(sys.maxsize), sys.maxsize) # Default state attributes set.
+
+    config_init_state(arguments,state,computer,human) # Reconfigure state attributes based on the passed arguments
+
+    print("\n_______________________________Nim Game_______________________________")
+    while((state.remainBMarbles>0) and (state.remainRMarbles>0)):
+        if((state.player).entity == "computer"):
+            print("\n=====Computer=====")
+
+            action = alpha_beta_decision(state)
+            state = take_action(state, action, computer, human)
+
+            state.player = human
+
+        else: #player turn
+            print("\n=====Player=====")
+            print_marble_stacks(state)
+
+            marble_color = query_marble_color()
+            marbles_taken = query_marbles_taken(state, marble_color)
+
+            action = Action(marble_color, marbles_taken)
+
+            state = take_action(state,action,computer,human)
+    end_of_game(state)
+
+
+if __name__ == "__main__":
+    main()
 
 # - Arguments:
 # 	- num red (req)
