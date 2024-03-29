3d18e76f8a47d823c78fc8e09c79b8f3de6f9a41
isa-me <medranoisaac0875@gmail.com>
2024-03-29 02:19:46 -0500

    potentially working alpha-beta algorithm

--- Assignment_2/red_blue_nim.py
+++ Assignment_2/red_blue_nim.py
@@ -1,125 +1,57 @@
 import sys
 
 class State:
-    def __init__(self, remainBMarbles, remainRMarbles, version, player, depth, computer, human, alpha, beta):
-        self.remainBMarbles = int(remainBMarbles)
+    def __init__(self, remainRMarbles, remainBMarbles):
         self.remainRMarbles = int(remainRMarbles)
-        self.version = version
-        self.player = player
-        self.depth = int(depth)
-        self.computer = computer
-        self.human = human
-        self.alpha = int(alpha)
-        self.beta = int(beta) 
-
-class Player:
-    def __init__(self, entity, playerBMarbles, playerRMarbles):
-        self.entity = entity
-        self.playerBMarbles = int(playerBMarbles)
-        self.playerRMarbles = int(playerRMarbles)
-
+        self.remainBMarbles = int(remainBMarbles)
+        
 class Action:
     def __init__(self, marble_color, marbles_taken):
         self.marble_color = marble_color
         self.marbles_taken = int(marbles_taken)
         self.utility = 0
 
-def player_score(player):
-    score = (player.playerBMarbles*3) + (player.playerRMarbles*2) 
-    return score
+def make_copy_state(state):   
+    return State(state.remainRMarbles, state.remainBMarbles)
 
-def take_action(state,action,computer,human): # After taking an action, the game state changes. Configure the game state 
+def take_action(action,state,current_player): # After taking an action, the game state changes. Configure the game state 
     # Take Action Message
-    if((action.marble_color == "B") and ((state.player).entity == "computer")):
-        if(action.marbles_taken == 1):
+    if((action.marble_color == "R") and (current_player == "computer")):
+            print("Computer takes", action.marbles_taken, "red marbles.\n")    
+    elif((action.marble_color == "B") and (current_player == "computer")):
             print("Computer takes", action.marbles_taken, "blue marble.\n")
-        else:
-            print("Computer takes", action.marbles_taken, "blue marbles.\n")
-    elif((action.marble_color == "R") and ((state.player).entity == "computer")):
-        if(action.marbles_taken == 1):
-            print("Computer takes", action.marbles_taken, "red marble.\n")
-        else:
-            print("Computer takes", action.marbles_taken, "red marbles.\n")
-    elif((action.marble_color == "B") and ((state.player).entity == "human")):
-        if(action.marbles_taken == 1):
-            print("You take", action.marbles_taken, "blue marble.\n")
-        else:
-            print("You take", action.marbles_taken, "blue marbles.\n")
-    elif((action.marble_color == "R") and ((state.player).entity == "human")):
-        if(action.marbles_taken == 1):
-            print("You take", action.marbles_taken, "red marble.\n")
-        else:
+    elif((action.marble_color == "R") and (current_player == "human")):
             print("You take", action.marbles_taken, "red marbles.\n")
+    elif((action.marble_color == "B") and (current_player == "human")):
+            print("You take", action.marbles_taken, "blue marbles.\n")
 
-    # Things that will change: remaining marbles, player marbles, and the current player, alpha and beta values set to -infinity and +infinity for computer's turn
+    # Things that will change: remaining marbles are the only thing that change from state to state
     if((action.marble_color == "B")): # Marble picked is blue
         state.remainBMarbles = state.remainBMarbles - action.marbles_taken
     else: # Marble picked is red
         state.remainRMarbles = state.remainRMarbles - action.marbles_taken
-    if((state.player).entity == "computer"): # current player is computer, next turn is human.
-        state.player = human
-    else: # current player is human, next turn is computer.
-        state.player = computer
-
-    state.alpha = -(sys.maxsize)
-    state.beta = sys.maxsize
-
-    return state
 
 def print_marble_stacks(state):
-    print("-------------------------------")   
-    print("Remaining Blue Marbles: " , state.remainBMarbles)
+    print("-------------------------------")
     print("Remaining Red Marbles: " , state.remainRMarbles)
-    print("-------------------------------\n")   
+    print("Remaining Blue Marbles: " , state.remainBMarbles)
+    print("-------------------------------\n")
 
 def is_integer(s):
     return s.isdigit()
 
-def config_init_state(arguments,state,computer,human):
-    if(len(arguments)!=2): # User gave more than bare minimum arguments, must reconfigure default state values for <version> and <first-player> .
-        if(len(arguments)==3): # User gave one optional argument, don't consider default attributes case.
-            if((arguments[2]=="human")or(arguments[2]=="Human")):
-                state.player = human
-            elif(arguments[2]=="misere")or(arguments[2]=="Misere"):
-                state.version = "misere"
-            elif(is_integer(arguments[2])):
-                state.depth = arguments[2]
-        elif(len(arguments)==4): # User gave atleast 2 optional arguments, don't configure default attributes case.
-            if((arguments[2]=="misere")or(arguments[2]=="Misere") and (arguments[3]=="human")or(arguments[3]=="Human")):
-                state.version = "misere"
-                state.player = human
-            elif((arguments[2]=="misere")or(arguments[2]=="Misere") and (arguments[3]=="computer")or(arguments[3]=="Computer")):  
-                state.version = "misere"
-            elif((arguments[2]=="standard")or(arguments[2]=="Standard") and (arguments[3]=="human")or(arguments[3]=="Human")):  
-                state.player = human
-            elif((arguments[2]=="misere")or(arguments[2]=="Misere") and is_integer(arguments[3])):
-                state.version = "misere"
-                state.depth = arguments[3]
-            elif((arguments[2]=="human")or(arguments[2]=="Human") and is_integer(arguments[3])):
-                state.player = human
-                state.depth = arguments[3]
-        elif(len(arguments)==5): # User gave all arguments.
-            state.depth = int(arguments[4])
-            if((arguments[2]=="misere")or(arguments[2]=="Misere") and (arguments[3]=="human")or(arguments[3]=="Human")):
-                state.version = "misere"
-                state.player = human
-            elif((arguments[2]=="misere")or(arguments[2]=="Misere") and (arguments[3]=="computer")or(arguments[3]=="Computer")):  
-                state.version = "misere"
-            elif((arguments[2]=="standard")or(arguments[2]=="Standard") and (arguments[3]=="human")or(arguments[3]=="Human")):  
-                state.player = human
-
 def query_marble_color():
-    marble_color = input("Choose Marble (B/R): ").rstrip()
-    if(marble_color == "b"):
+    marble_color = input("Choose Marble (R/B): ").rstrip()
+    if(marble_color == "r"):
+        marble_color = "R"    
+    elif(marble_color == "b"):
         marble_color = "B"
-    elif(marble_color == "r"):
-        marble_color = "R"
-    while((marble_color != "B") and (marble_color != "R")):
-        marble_color = input("Choose Marble (B/R): ").rstrip()
-        if(marble_color == "b"):
+    while((marble_color != "R") and (marble_color != "B")):
+        marble_color = input("Choose Marble (R/B): ").rstrip()
+        if(marble_color == "r"):
+            marble_color = "R"        
+        elif(marble_color == "b"):
             marble_color = "B"
-        elif(marble_color == "r"):
-            marble_color = "R"
     return marble_color
 
 def query_marbles_taken(state, marble_color):
@@ -130,100 +62,122 @@ def query_marbles_taken(state, marble_color):
     else:
         while((marbles_taken != "1") and (marbles_taken != "2") or (state.remainRMarbles < int(marbles_taken))):
             marbles_taken = input("Choose Amount (1/2): ").rstrip()
-    
     return marbles_taken
 
-def end_of_game(state):
-    if(((state.player).entity == "computer") and (state.version == "standard")):
+def end_of_game(state,current_player,version):
+    if((current_player == "computer") and (version == "standard")):
         print("You Won")
-    elif(((state.player).entity == "human") and (state.version == "standard")):
+    elif((current_player == "human") and (version == "standard")):
         print("Game Over")
-    elif(((state.player).entity == "computer") and (state.version == "misere")):
+    elif((current_player == "computer") and (version == "misere")):
         print("Game Over")
-    elif(((state.player).entity == "human") and (state.version == "misere")):
+    elif((current_player == "human") and (version == "misere")):
         print("You Won")
 
 def successor_state(action, state): # works similarily like take_action(), used in the alpha-beta decision algorithm for the computer's decision. Returns the successor state after taking an action
-    computer = Player("computer", (state.computer).playerBMarbles, (state.computer).playerRMarbles)
-    human = Player("human", (state.human).playerBMarbles, (state.human).playerRMarbles)
+    if(action.marble_color == "R"):
+        successor = State(state.remainRMarbles - action.marbles_taken, state.remainBMarbles)    
+    elif(action.marble_color == "B"):
+        successor = State(state.remainRMarbles, state.remainBMarbles - action.marbles_taken)
 
-    if((state.player).entity == "human"): # current player is human, successor state is computer's turn.
-        successor = State(state.remainBMarbles, state.remainRMarbles, state.version, computer, state.depth+1, computer, human, state.alpha, state.beta)
-    else: # current player is human, successor state is computer's turn.
-        successor = State(state.remainBMarbles, state.remainRMarbles, state.version, human, state.depth+1, computer, human, state.alpha, state.beta)
-
-    if((action.marble_color == "B")): # Marble picked is blue
-        successor.remainBMarbles = successor.remainBMarbles - action.marbles_taken
-    else: # Marble picked is red
-        successor.remainRMarbles = successor.remainRMarbles - action.marbles_taken
 
     return successor
 
-def possible_actions(state)
-    # 
+def possible_actions(state):
+    actions = []
 
-def successors(state)
-    actionsList = possible_actions(state) # returns a list of possible actions for a given state
+    # Case 1: 4 available options (R2, B2, R1, B1) <- The general case
+    if((state.remainBMarbles>1) and (state.remainRMarbles>1)):
+        actions.append(Action("R","2"))
+        actions.append(Action("B","2"))
+        actions.append(Action("R","1"))
+        actions.append(Action("B","1"))
+        
+    # Case 2: 3 available options (R2, R1, B1) <- Edge case, there's only one blue marble left
+    elif((state.remainBMarbles==1) and (state.remainRMarbles>1)):
+        actions.append(Action("R","2"))
+        actions.append(Action("R","1"))
+        actions.append(Action("B","1"))
+        
+    # Case 3: 3 available options (B2, R1, B1) <- Edge case, there's only one red marble left
+    elif((state.remainBMarbles>1) and (state.remainRMarbles==1)):
+        actions.append(Action("B","2"))
+        actions.append(Action("R","1"))
+        actions.append(Action("B","1"))
+        
+    # Case 4: 2 available options (R1, B1) <Edge case, there's only one red and one blue marble left
+    elif((state.remainBMarbles==1) and (state.remainRMarbles==1)):
+        actions.append(Action("R","1"))
+        actions.append(Action("B","1"))
+
+    return actions
+
+def successors(state):
+    actionsList = possible_actions(state) # returns a list of possible actions and a corresponding list of successor states
     successorStateList = []
     for action in actionsList: 
-        successorStateList.apend(successor_state(action,state))
+        successorStateList.append(successor_state(action,state))
     return successorStateList
 
 def utility(state):
+    return (state.remainRMarbles * 2) + (state.remainBMarbles * 3)
 
-def terminal_test(test):
-
-def max_util(actions): # returns the higest utility among a list of actions
-
-# def min_util(actions):
-
-def find(util, actions) # Maps the given utility to the first corresponding action in the action list. Returns found action
-
-def max_value(state):
-    util = utility(state)
-    if (terminal_test(state) == True)
-        return util
+def terminal_test(state):
+    if ((state.remainRMarbles == 0) and (state.remainBMarbles == 0)):
+        print(" Error in terminal_test(): remaining blue and red marbles are both zero", end=" ")
+        return False
+    if ((state.remainRMarbles == 0) or (state.remainBMarbles == 0)):
+        return True
+    else: 
+        return False
+# =====================================================================================================
+def max_value(state, alpha, beta): 
+    if (terminal_test(state) == True):
+        return utility(state)
     v = -(sys.maxsize)
 
     successorStateList = successors(state)
 
     for s in successorStateList:
-        v = max(v, min_value(s))
-        if(v>=state.beta)
+        v = max(v, min_value(s,alpha,beta))
+        if(v>=beta):
             return v
-        state.alpha = max(state.alpha,v)
+        alpha = max(alpha,v)
+        # alpha.value = max(alpha.value,v)
     
     return v
 
-def min_value(state):
-    util = utility(state)
-    if (terminal_test(state) == True)
-        return util
+def min_value(state, alpha, beta):
+    if (terminal_test(state) == True):
+        return utility(state)
     v = sys.maxsize
 
     successorStateList = successors(state)
 
     for s in successorStateList:
-        v = min(v, max_value(s))
-        if(v<=state.alpha)
+        v = min(v, max_value(s,alpha,beta))
+        if(v<=alpha):
             return v
-        state.beta = min(state.beta,v)
+        beta = min(beta,v)
+        # beta.value = min(beta.value,v)
     
-    return v
+    return v  
 
 def alpha_beta_decision(state):
-    # Configure the alpha and beta values
-    state.alpha = -(sys.maxsize)
-    state.beta = sys.maxsize
+    v = max_value(make_copy_state(state),-(sys.maxsize),sys.maxsize)
+    print("Winning utiltity is", v)
 
-    actionsList = possible_actions(state) # returns a list of possible actions for a given state
-    for action in actionsList: 
-        action.utility = min_value(successor_state(action, state)) # for each possible action we configure it's payoff (utility)
-    utility = max_util(actionsList) # returns the higest utility among the actions in actionList
-    return find(util, actionsList) # returning the action with the highest utility
+    actionsList = possible_actions(make_copy_state(state)) # returns a list of possible actions for a given state
+    utilities = []
+    for action in actionsList:       
+        utility = min_value(successor_state(action, make_copy_state(state)),-(sys.maxsize),sys.maxsize)
+        utilities.append(utility)
+        print("Utility for Action: " + str(action.marble_color) + str(action.marbles_taken)+ " is " + str(utility))
 
-
-
+    for a, u, in zip(actionsList,utilities):
+        if u == v:
+            return a
+# =====================================================================================================
 
 
 def main():
@@ -231,24 +185,35 @@ def main():
         print("Not enough arguments\n")
         sys.exit()
 
-    arguments = sys.argv[1:] # Arguments passed: <num red> <num blue> <version> <first-player> <depth>.
-    computer = Player("computer", 0, 0)
-    human = Player("human", 0, 0)
-    state = State(arguments[0], arguments[1], "standard", computer, 0, computer, human, -(sys.maxsize), sys.maxsize) # Default state attributes set.
+    arguments = sys.argv[1:] # Arguments passed: <num red> <num blue> <version> <first-player> <depth>.)
 
-    config_init_state(arguments,state,computer,human) # Reconfigure state attributes based on the passed arguments
+    state = State(arguments[0], arguments[1]) # Default state attributes set.
+
+    if((arguments[2] == "standard") or (arguments[2] == "Standard")):
+        version = "standard"
+    elif((arguments[2] == "misere") or (arguments[2] == "Misere")):
+        version = "misere"
+
+    if((arguments[3] == "computer") or (arguments[3] == "Computer")):
+        current_player = "computer"
+    elif((arguments[3] == "human") or (arguments[3] == "Human")):
+        current_player = "human"
+    
+
 
     print("\n_______________________________Nim Game_______________________________")
     while((state.remainBMarbles>0) and (state.remainRMarbles>0)):
-        if((state.player).entity == "computer"):
+        if(current_player == "computer"):
             print("\n=====Computer=====")
+            print_marble_stacks(state)
+            action = alpha_beta_decision(make_copy_state(state))
+            take_action(action, state, current_player)
 
-            action = alpha_beta_decision(state)
-            state = take_action(state, action, computer, human)
+            # NOTE: I may have to make a copy of the state before passing into alpha_beta_decision()
 
-            state.player = human
+            current_player = "human"
 
-        else: #player turn
+        elif(current_player == "human"): #player turn
             print("\n=====Player=====")
             print_marble_stacks(state)
 
@@ -257,30 +222,11 @@ def main():
 
             action = Action(marble_color, marbles_taken)
 
-            state = take_action(state,action,computer,human)
-    end_of_game(state)
+            take_action(action,state,current_player)
+            current_player = "computer"
+    end_of_game(state,current_player,version)
+
 
 
 if __name__ == "__main__":
     main()
-
-# - Arguments:
-# 	- num red (req)
-# 	- num blue (req)
-# 	- version (standard or misere)
-# 	- first-player (human or computer, computer is the default option)
-# 	- depth (for extra credit)
-# - Switch b/w player and opponent turns
-# 	- on turn, pick a pile
-# 	- on turn, remove one marble
-# 	- on turn, remove two marbles
-# - two stacks of marbles, red and blue marbles
-# 	- amount given in arguments
-# 	- red marbles worth 2
-# 	- blue marbles worth 3
-# - standard and misere ver
-# 	- loss condition in standard: either pile is empty on player's turn.
-# 	- win condition in misere ver: either pile is empty on player's turn.
-# 	- remaining marbles determine winning points
-# - computer opponent is the optimal player
-# 	- MinMax w/ Alpha Beta Prunning 
